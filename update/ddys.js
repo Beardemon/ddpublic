d75d2faf63644ca2555d9401e523a658dba0ce62
Licz <ykslcz@163.com>
2022-08-24 23:32:39 +0800

    Update ddys.js

--- update/ddys.js
+++ update/ddys.js
@@ -1,10 +1,10 @@
 {
   "needUpdate":true,
-  "url":"https://diandiango.coding.net/p/ddPublicFile/d/ddPublicFile/git/raw/master/raw/diandianyingshi_9.5.2.apk",
-  "des":"\n1、修复x5播放器，使用内置播放器失败会自动用x5播放(播放器兼容性大大提升)\n2、增加追更功能，第一时间知道追的剧更了没\n3、修复解析源的重试机制\n4、首页ui微调，把没用的数据往后调\n5、修复部分源下一页数据重复问题\n6、exo播放器适配后台逻辑，可以不在频繁被杀进程",
-  "constraint":true,
-  "size":"9.9M",
-  "version":952,
-  "versionName":"9.5.2",
-  "backWebUrl":"https://wwi.lanzoui.com/ddys"
+  "url":"https://cdn.githubjs.cf/Beardemon/ddpublic/blob/main/raw/ddys955_nocore.apk?raw=true",
+  "des":"\n1、修复由于国内监管原因导致的各种平台不能用从而源全部消失的问题，能不能看到这个更新看造化了！！！",
+  "constraint":false,
+  "size":"10.3M",
+  "version":955,
+  "versionName":"9.5.5",
+  "backWebUrl":"https://wwi.lanzoup.com/ddysnox5"
 }
