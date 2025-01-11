a9a5b45f4474427bf1747108f0eef99bbb768a26
licz <ykslcz@163.com>
2025-01-11 14:03:06 +0800

    更新961

---
+++ update/ddys_961.js
@@ -0,0 +1,10 @@
+{
+    "needUpdate":true,
+    "url":"https://pan.tenire.com/down.php/638431b55b5a75f38556da2e388373a5.apk",
+    "des":"\n1、更新了获取源的一些规则，更新的话能用的源会变多。",
+    "constraint":false,
+    "size":"12.3M",
+    "version":961,
+    "versionName":"9.6.1",
+    "backWebUrl":"https://wwi.lanzoup.com/ddys"
+}
